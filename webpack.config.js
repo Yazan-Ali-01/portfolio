9cb94b086fc4d7c1d310106e7919713bb5e64ad9
Yazan Ali <yazan.ali.dev@gmail.com>
2024-05-30 00:10:54 +0400

    fixed routing in gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
+    publicPath: '/home/',
     clean: true, // Clean the build folder before each build
   },
   module: {
